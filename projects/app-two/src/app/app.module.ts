b02f77f2d7da426a965cf920b7908f85fd1abf6a
brainspork <anthony.konschuh@gmail.com>
2022-12-03 08:59:42 -0800

    set up home page two

--- projects/app-two/src/app/app.module.ts
+++ projects/app-two/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageModule } from './pages/home-page/home-page.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +10,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    // pages
+    HomePageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
